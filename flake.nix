53dd23cf3712a0025c56daa7a1bbf73f549f1d20
christian <>
2022-12-24 03:51:56 -0700

    Refactored 'x86_64-linux' --> system

--- flake.nix
+++ flake.nix
@@ -32,100 +32,105 @@
       sqliteDocDir = "sqlite-doc-${serialisedVersion}";
       
   in rec {
-    packages.x86_64-linux.default = pkgs.stdenv.mkDerivation {
-        pname = "sqlite3";
-        inherit version;
+    packages.${system} = {
+        default = pkgs.stdenv.mkDerivation {
+            pname = "sqlite3";
+            inherit version;
 
-        srcs = [ fetchSqliteAutoconf ];
-        sourceRoot = "./${sqliteSrcDir}";
+            srcs = [ fetchSqliteAutoconf ];
+            sourceRoot = "./${sqliteSrcDir}";
 
-        outputs = [ "out" ];
+            outputs = [ "out" ];
 
-        nativeBuildInputs = 
-            [ 
-                # `readline` makes the CLI interface usable
-                pkgs.readline 
-            ];
+            nativeBuildInputs = 
+                [ 
+                    # `readline` makes the CLI interface usable
+                    pkgs.readline 
+                ];
 
-        configureFlags = 
-            [
-                # Some awkwardness to refer to the environment variable `$out`.
-                # See [here](https://nixos.org/manual/nix/stable/release-notes/rl-2.0.html)
-                "--prefix=${builtins.placeholder "out"}" 
-                "--enable-readline"
-                "--enable-threadsafe"
-                "--enable-math"
-                "--enable-fts4"
-                "--enable-fts5"
-            ];
-    };
+            configureFlags = 
+                [
+                    # Some awkwardness to refer to the environment variable `$out`.
+                    # See [here](https://nixos.org/manual/nix/stable/release-notes/rl-2.0.html)
+                    "--prefix=${builtins.placeholder "out"}" 
+                    "--enable-readline"
+                    "--enable-threadsafe"
+                    "--enable-math"
+                    "--enable-fts4"
+                    "--enable-fts5"
+                ];
+        };
 
-    # HTML sqlite documentation
-    packages.x86_64-linux.sqlite-doc = pkgs.stdenv.mkDerivation {
-        pname = "sqlite3-doc";
-        inherit version;
+        # HTML sqlite documentation
+        sqlite-doc = pkgs.stdenv.mkDerivation {
+            pname = "sqlite3-doc";
+            inherit version;
 
-        srcs = [ fetchSqliteDoc ];
-        sourceRoot = "${sqliteDocDir}";
+            srcs = [ fetchSqliteDoc ];
+            sourceRoot = "${sqliteDocDir}";
 
-        outputs = [ "out" ];
+            outputs = [ "out" ];
 
-        nativeBuildInputs = 
-            [ 
-                # `unzip` is needed for grabbing the HTML docs
-                pkgs.unzip 
-            ];
+            nativeBuildInputs = 
+                [ 
+                    # `unzip` is needed for grabbing the HTML docs
+                    pkgs.unzip 
+                ];
 
-        # Modifying the phases.
-        # See [nixpkgs documentation](https://nixos.org/manual/nixpkgs/stable/#sec-using-stdenv)
-        # See
-        # [stdenv](https://github.com/NixOS/nixpkgs/blob/master/pkgs/stdenv/generic/setup.sh)
-        # implementation as well.
-        # Debugging notes:
-        #   - Enter `nix develop`
-        #   - Run `genericBuild`
-        installPhase = ''
-            set -e
-            runHook preInstall
+            # Modifying the phases.
+            # See [nixpkgs documentation](https://nixos.org/manual/nixpkgs/stable/#sec-using-stdenv)
+            # See
+            # [stdenv](https://github.com/NixOS/nixpkgs/blob/master/pkgs/stdenv/generic/setup.sh)
+            # implementation as well.
+            # Debugging notes:
+            #   - Enter `nix develop`
+            #   - Run `genericBuild`
+            installPhase = ''
+                set -e
+                runHook preInstall
 
-            mkdir -p $out/share/doc
-            cp -r .  $out/share/doc
-            
-            # Check if `index.html` exists
-            file "$out/share/doc/index.html"
+                mkdir -p $out/share/doc
+                cp -r .  $out/share/doc
+                
+                # Check if `index.html` exists
+                file "$out/share/doc/index.html"
 
-            runHook postInstall
+                runHook postInstall
+            '';
+        };
+
+        # Convienent shell function to open documentation similar to `nixos-help`
+        sqlite-help = pkgs.writeShellScriptBin "sqlite-help" ''
+          # Finds first executable browser in a colon-separated list.
+          # (see how xdg-open defines BROWSER)
+          browser="$(
+            IFS=: ; for b in $BROWSER; do
+              [ -n "$(type -P "$b" || true)" ] && echo "$b" && break
+            done
+          )"
+          if [ -z "$browser" ]; then
+            browser="$(type -P xdg-open || true)"
+            if [ -z "$browser" ]; then
+              browser="${pkgs.w3m-nographics}/bin/w3m"
+            fi
+          fi
+          exec "$browser" "${packages."${system}".sqlite-doc + "/share/doc/index.html"}"
         '';
     };
 
-    # Convienent shell function to open documentation similar to `nixos-help`
-    packages.x86_64-linux.sqlite-help = pkgs.writeShellScriptBin "sqlite-help" ''
-      # Finds first executable browser in a colon-separated list.
-      # (see how xdg-open defines BROWSER)
-      browser="$(
-        IFS=: ; for b in $BROWSER; do
-          [ -n "$(type -P "$b" || true)" ] && echo "$b" && break
-        done
-      )"
-      if [ -z "$browser" ]; then
-        browser="$(type -P xdg-open || true)"
-        if [ -z "$browser" ]; then
-          browser="${pkgs.w3m-nographics}/bin/w3m"
-        fi
-      fi
-      exec "$browser" "${packages.x86_64-linux.sqlite-doc + "/share/doc/index.html"}"
-    '';
 
     # Amalmagates the above derivations. For details on `symlinkJoin`, see
     # [here](https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/trivial-builders.nix)
-    devShells.x86_64-linux.default = pkgs.symlinkJoin {
+    devShells.${system} = {
+        default = pkgs.symlinkJoin {
         name = "sqlite-dev-shell";
         paths = 
             [ 
-                packages.x86_64-linux.default
-                packages.x86_64-linux.sqlite-doc
-                packages.x86_64-linux.sqlite-help
+                packages.${system}.default
+                packages.${system}.sqlite-doc
+                packages.${system}.sqlite-help
             ];
+        };
     };
   };
 }
